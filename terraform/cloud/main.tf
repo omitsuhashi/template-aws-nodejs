2dc5fbc8ad7e9fa3d51706ad7fbb44647440624c
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2024-03-22 06:27:57 +0900

    🔥remove service name

--- terraform/cloud/main.tf
+++ terraform/cloud/main.tf
@@ -2,22 +2,13 @@ terraform {
   required_version = ">= 1.7.4"
   backend "s3" {
     bucket = "deploy-management-bucket"
-    key    = "life-log/terraform.tfstate"
+    key    = "{service_name}/terraform.tfstate"
     region = "ap-northeast-1"
   }
 }
 
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "localstack"
-  secret_key = "localstack"
-
-  endpoints {
-    s3         = "http://s3.localhost.localstack.cloud:4566"
-    lambda     = "http://localhost:4566"
-    apigateway = "http://localhost:4566"
-    sts        = "http://localhost:4566"
-  }
 }
 
 module "main" {
